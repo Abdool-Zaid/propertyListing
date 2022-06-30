6dbc996e3403857136e6a7930d96bd1a95952d89
unknown <aztoorabally7447@gmail.com>
2022-06-30 15:01:37 +0200

    update

--- admin.js
+++ admin.js
@@ -2,9 +2,9 @@ let listing = JSON.parse(localStorage.getItem("listing"));
 let retrievedList = localStorage.getItem("listing");
 let RetrievedArray = JSON.parse(localStorage.getItem("listing"));
 display = () => {
-    document.querySelector("#target").innerHTML = "";
-    JSON.parse(retrievedList).forEach((Planet) => {
-        document.querySelector("#target").innerHTML += `
+  document.querySelector("#target").innerHTML = "";
+  JSON.parse(retrievedList).forEach((Planet) => {
+    document.querySelector("#target").innerHTML += `
         <div class="Item"  onclick='EditItem()'  >
         <img src="${Planet.image}"  alt="PLanet IMage">  
         <img src="${Planet.populationURL}" alt="No lifeforms known">
@@ -24,7 +24,7 @@ display = () => {
         </div>
         </div>
         `;
-    });
+  });
 };
 display();
 EditItem = () => {};
@@ -33,12 +33,50 @@ deleteItem = () => {
     console.log(Planet);
     Planet.id !== this.id;
     console.log("running");
-    
-});
-// localStorage.listing = 0;
-localStorage.setItem("listing", JSON.stringify(listing));
-display();
+  });
+  // localStorage.listing = 0;
+  localStorage.setItem("listing", JSON.stringify(listing));
+  display();
 };
 AddToArray = () => {
-  let test = prompt("aaaaa");
+  // id: 14,
+  // name: "Ajian",
+  // atmosphere: "ample",
+  // moons: "5",
+  // tempRange: "habitable",
+  // water: "liquid",
+  // size: "large",
+  // population: "colonial",
+  // populationURL:
+  //   "https://raw.githubusercontent.com/Abdool-Zaid/propertyListing/main/assets/aliens/download%20(28).png",
+  // image:
+  //   "https://raw.githubusercontent.com/Abdool-Zaid/propertyListing/main/assets/planetImages/download%20(14).png",
+  // price: (Math.rando
+  let Array = localStorage.listing;
+  let id = Array.length + 1;
+  let name = prompt("enter planets name");
+  let atmosphere = prompt("enter atmospheric condition");
+  let moons = prompt("enter amount of moons");
+  let tempRange = prompt("enter tempRange");
+  let water = prompt("enter water state");
+  let size = prompt("enter size");
+  let population = prompt("enter population");
+  let populationURL = prompt("species image url");
+  let image = prompt("planet image url");
+  let price = (Math.random() * 40000).toFixed(2);
+  newArray = listing.push({
+    id: id,
+    name: name,
+    atmosphere: atmosphere,
+    moons: moons,
+    tempRange: tempRange,
+    water: water,
+    size: size,
+    population: population,
+    populationURL: populationURL,
+    image: image,
+    price: price,
+  });
+  localStorage.setItem("listing", JSON.stringify(newArray));
+  window.location.reload();
 };
