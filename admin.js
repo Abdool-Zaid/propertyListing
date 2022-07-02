45fb89e2ab38ad4a7ea5b158294166ec9cab2fc0
Abdool-Zaid <aztoorabally7447@gmail.com>
2022-07-02 17:46:22 +0200

    delete

--- admin.js
+++ admin.js
@@ -5,45 +5,52 @@ display = () => {
   document.querySelector("#target").innerHTML = "";
   JSON.parse(retrievedList).forEach((Planet) => {
     document.querySelector("#target").innerHTML += `
-        <div class="Item"  onclick='EditItem()'  >
-        <img src="${Planet.image}"  alt="PLanet IMage">  
-        <img src="${Planet.populationURL}" alt="No lifeforms known">
-        <h1>${Planet.name}</h1>
-        <div class ="subinfo">
-        <p  >atmospheric conditions: ${Planet.atmosphere}</p>
-        <p >Moons: ${Planet.moons}</p>
-        <p >Weather: ${Planet.tempRange}</p>
-        <p > water: ${Planet.water}</p>
-        <p > Size: ${Planet.size}</p>
-        <p > population: ${Planet.population}</p>
-        <p class="subInfo" dir="rtl"> JWT: ${Planet.price}</p>
-        </div>
-        <div class ="editor">
-        <button onclick="EditItem()" id="${Planet.id}">edit</button>
-        <button class="deleteItem" id="${Planet.id}">delete</button>
-        </div>
-        </div>
-        `;
+    <div class="Item"  onclick='EditItem()'  >
+    <img src="${Planet.image}"  alt="PLanet IMage">  
+    <img src="${Planet.populationURL}" alt="No lifeforms known">
+    <h1>${Planet.name}</h1>
+    <div class ="subinfo">
+    <p  >atmospheric conditions: ${Planet.atmosphere}</p>
+    <p >Moons: ${Planet.moons}</p>
+    <p >Weather: ${Planet.tempRange}</p>
+    <p > water: ${Planet.water}</p>
+    <p > Size: ${Planet.size}</p>
+    <p > population: ${Planet.population}</p>
+    <p class="subInfo" dir="rtl"> JWT: ${Planet.price}</p>
+    </div>
+    <div class ="editor">
+    <button onclick="EditItem()" id="${Planet.id}">edit</button>
+    <button class="deleteItem" id="${Planet.id}">delete</button>
+    </div>
+    </div>
+    `;
   });
 };
-display();
-EditItem = () => {};
-
-document.querySelectorAll(".deleteItem").forEach((event) => {
-  event.onclick = (event) => {
-    let targetID = event.currentTarget.id;
-    console.log(targetID);
-    console.log(listing);
-    let leanArray= listing.splice(targetID-1,1)
-    console.log(leanArray)
-    console.log(listing);
-    localStorage.clear()
-  
-    localStorage.setItem("listing", JSON.stringify(listing));
-
-  
-  };
-});
+    display();
+    EditItem = () => {};
+    
+    document.querySelectorAll(".deleteItem").forEach((event) => {
+      event.onclick = (event) => {
+        let targetID = event.currentTarget.id;
+        console.log(targetID);
+        console.log(listing);
+        let leanArray= listing.splice(targetID-1,1)
+        console.log(leanArray)
+        console.log(listing);
+        localStorage.clear()
+        localStorage.setItem("listing", JSON.stringify(listing));
+        document.querySelector("#target").innerHTML = "";
+        document.querySelector("#target").innerHTML +=`
+        <div class="deletedItem"  >
+        <h1>${leanArray[0].name} has been deleted</h1>
+        <h2>ID: ${leanArray[0].id}</h2>
+        <img src="${leanArray[0].image}"  alt="PLanet IMage">  
+      
+        `;
+        
+        
+      };
+    });
 
 runAdd = () => {
   document.querySelector("#target").innerHTML = "";
